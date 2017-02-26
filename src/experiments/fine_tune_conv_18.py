6ac4a0b51804f3cd7271f86ce06039dc644fd839
dixe <niko.fris@gmail.com>
2017-02-26 14:38:59 +0100

    fined_tune_model_18

--- src/experiments/fine_tune_conv_18.py
+++ src/experiments/fine_tune_conv_18.py
@@ -9,35 +9,6 @@ In our setup, we:
 - created a data/ folder
 - created train/ and validation/ subfolders inside data/
 - created cats/ and dogs/ subfolders inside train/ and validation/
-- put the cat pictures index 0-999 in data/train/cats
-- put the cat pictures index 1000-1400 in data/validation/cats
-- put the dogs pictures index 12500-13499 in data/train/dogs
-- put the dog pictures index 13500-13900 in data/validation/dogs
-
-So that we have 1000 training examples for each class, and 400 validation examples for each class.
-
-In summary, this is our directory structure:
-```
-data/
-    train/
-        dogs/
-            dog001.jpg
-            dog002.jpg
-            ...
-        cats/
-            cat001.jpg
-            cat002.jpg
-            ...
-    validation/
-        dogs/
-            dog001.jpg
-            dog002.jpg
-            ...
-        cats/
-            cat001.jpg
-            cat002.jpg
-            ...
-```
 '''
 
 import os
@@ -50,9 +21,8 @@ from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 
 # path to the model weights file.
-weights_path = 'vgg16_weights.h5'
-top_model_weights_path = 'bottleneck_fc_model.h5'
-fine_tuned_weights = 'fine_tuned_model.h5'
+weights_path = 'fine_tuned_model.h5'
+fine_tuned_weights = 'fine_tuned_model_18.h5'
 # dimensions of our images.
 img_width, img_height = 64, 64
 
@@ -120,18 +90,6 @@ def get_model():
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
     #31
 
-    # load weights for finetuned model 25
-    assert os.path.exists(weights_path), 'Model weights not found (see "weights_path" variable in script).'
-    f = h5py.File(weights_path)
-    for k in range(f.attrs['nb_layers']):
-        if k >= len(model.layers):
-            # we don't look at the last (fully-connected) layers in the savefile
-            break
-        g = f['layer_{}'.format(k)]
-        weights = [g['param_{}'.format(p)] for p in range(g.attrs['nb_params'])]
-        model.layers[k].set_weights(weights)
-    f.close()
-    print('Model loaded.')
 
     # build a classifier model to put on top of the convolutional model
     top_model = Sequential()
@@ -140,15 +98,16 @@ def get_model():
     top_model.add(Dropout(0.5))
     top_model.add(Dense(1, activation='sigmoid'))
 
-    # note that it is necessary to start with a fully-trained
-    # classifier, including the top classifier,
-    # in order to successfully do fine-tuning
-    top_model.load_weights(top_model_weights_path)
-
     # add the model on top of the convolutional base
     model.add(top_model)
+    # load weights for finetuned model 25
+    assert os.path.exists(weights_path), 'Model weights not found (see "weights_path" variable in script).'
 
-    # set the first 25 layers (up to the last conv block)
+    model.load_weights(weights_path)
+    print 'Model loaded.'
+
+
+    # set the first 18 layers (up to the second last conv block)
     # to non-trainable (weights will not be updated)
     for layer in model.layers[:18]:
         layer.trainable = False
@@ -198,3 +157,7 @@ def train_model(model):
 def fine_tune_model():
     model = get_model()
     train_model(model)
+
+
+if __name__ == "__main__":
+    fine_tune_model()
