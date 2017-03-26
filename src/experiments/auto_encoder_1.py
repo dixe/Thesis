2fa42ff653df51db9446d0f7dc1776c3d98ae7f4
dixe <niko.fris@gmail.com>
2017-03-26 17:03:57 +0200

    Code for showing prediction on single img

--- src/experiments/auto_encoder_1.py
+++ src/experiments/auto_encoder_1.py
@@ -4,14 +4,14 @@ from keras.models import Sequential, Model
 from keras.layers import Input, UpSampling2D, Convolution2D, MaxPooling2D, ZeroPadding2D, RepeatVector, Reshape
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator
-from network import Base_network, default_settings
+from network import Base_network, Auto_encoder, default_settings
 from run_settings import Net_settings
 import numpy as np
 import Weightstore as ws
 import ftlayer as ftl
 
 
-class auto_encoder(Base_network):
+class auto_encoder(Auto_encoder):
 
     def __init__(self,settings):
 
@@ -41,7 +41,6 @@ class auto_encoder(Base_network):
         conv1 = Convolution2D(30, 3, 3, activation='relu', border_mode='same')
 
 
-
         model.add(conv1)
 
         model.add(conv0)
@@ -61,72 +60,7 @@ class auto_encoder(Base_network):
         return model
 
 
-        model = Sequential()
-        model.add(ftl.FTLayer(input_shape=(3, self.settings.img_width, self.settings.img_height)))
 
-        flat= 3*self.settings.img_width * self.settings.img_height
-        print flat
-        model.add(Reshape((3*self.settings.img_width * self.settings.img_height,)))
-        model.add(RepeatVector(10))
-
-        model.add(Reshape((30,self.settings.img_width, self.settings.img_height)))
-
-
-        conv1 = Convolution2D(30, 3, 3, activation='relu', border_mode='same')
-
-        model.add(conv1)
-        model.add(conv1)
-
-
-        model.add(Convolution2D(3,3,3, activation="sigmoid", border_mode='same'))
-        return model
-
-        exit()
-        x = conv1(x)
-
-        decoded = Convolution2D(3,3,3, activation="sigmoid", border_mode='same')(x)
-
-        model = Model(input_img, decoded)
-
-
-
-        print  self.has_weights()
-        if self.has_weights():
-            model.load_weights(self.settings.save_weights_path)
-            print "loaded_model"
-
-
-
-        return model
-
-
-    def train_model(self, model):
-        train_datagen = ImageDataGenerator(
-            rescale=1./255,
-            shear_range=0.2,
-            zoom_range=0.2,
-            horizontal_flip=True)
-
-        train_generator = train_datagen.flow_from_directory(
-            self.settings.train_data_dir,
-            target_size=(self.settings.img_height, self.settings.img_width),
-            class_mode=None)
-
-
-
-
-        for e in range(self.settings.nb_epoch):
-            print "Epoche " + str(e)
-
-            imgs = train_generator.next()
-            model.fit(imgs, imgs, 32, 1, 1)
-            for i in range(self.settings.nb_train_samples/32):
-                imgs = train_generator.next()
-                model.fit(imgs, imgs, 32, 1, 0)
-
-        for l in model.layers:
-            print l.get_weights()
-        return model, None
 
 
     def get_model_train(self):
@@ -138,6 +72,7 @@ class auto_encoder(Base_network):
 
     def model_name(self):
         return "auto_encoder_1"
+
 
     def description(self):
         return "Auto encoder that has same encoder layer settings simple_model"
--- src/experiments/test_network.py
+++ src/experiments/test_network.py
@@ -3,7 +3,7 @@ import run_settings as rs
 import json
 import Weightstore as ws
 import numpy as np
-
+import cv2
 
 def visualize_model(model):
     from keras.utils.visualize_util import plot
@@ -81,6 +81,15 @@ def evaluate_model_and_report(model):
 
     return res
 
+
+def predict_img_path(model, path):
+
+    img = cv2.imread(path)
+
+
+    print model.predict_img(img)
+
+
 def correct(name, pred):
     if name.startswith("broken"):
         return pred < 0.5
@@ -104,6 +113,8 @@ if __name__ == "__main__":
         callback = evaluate_model_and_report
 
 
+    if 'pred' in sys.argv:
+        callback =
     guid_substring = sys.argv[-1]
 
     settings = ws.get_settings(guid_substring)
