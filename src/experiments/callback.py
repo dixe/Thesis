e8694084de5c826d15eca019585c5970afe7c001
dixe <niko.fris@gmail.com>
2017-05-15 10:47:37 +0200

    early stopping callback

--- src/experiments/callback.py
+++ src/experiments/callback.py
@@ -1,6 +1,22 @@
 import keras
 import sys
 
+class EarlyStoppingByLossVal(Callback):
+    def __init__(self, monitor='loss', value=0.01, verbose=0):
+        super(Callback, self).__init__()
+        self.monitor = monitor
+        self.value = value
+        self.verbose = verbose
+
+    def on_epoch_end(self, epoch, logs={}):
+        current = logs.get(self.monitor)
+        if current is None:
+            print("Early stopping requires %s available!" % self.monitor)
+
+        elif current < self.value:
+            if self.verbose > 0:
+                print("Epoch %05d: early stopping THR" % epoch)
+            self.model.stop_training = True
 
 class Stop_callback(keras.callbacks.Callback):
 
