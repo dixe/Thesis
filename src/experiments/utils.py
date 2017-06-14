4da9d43663c267c5db512263555136546f867696
dixe <niko.fris@gmail.com>
2017-06-14 13:02:26 +0200

    Added color filters vis

--- src/experiments/utils.py
+++ src/experiments/utils.py
@@ -9,8 +9,8 @@ image from a set of samples or weights.
 CODE ORIGINAL FROM http://deeplearning.net/tutorial/utilities.html#how-to-plot
 """
 
-import numpy
-
+import numpy as np
+import cv2
 
 def scale_to_unit_interval(ndar, eps=1e-8):
     """ Scales all values in the ndarray ndar to be between 0 and 1 """
@@ -75,10 +75,10 @@ def tile_raster_images(X, img_shape, tile_shape, tile_spacing=(0, 0),
         assert len(X) == 4
         # Create an output numpy ndarray to store the image
         if output_pixel_vals:
-            out_array = numpy.zeros((out_shape[0], out_shape[1], 4),
+            out_array = np.zeros((out_shape[0], out_shape[1], 4),
                                     dtype='uint8')
         else:
-            out_array = numpy.zeros((out_shape[0], out_shape[1], 4),
+            out_array = np.zeros((out_shape[0], out_shape[1], 4),
                                     dtype=X.dtype)
 
         #colors default to 0, alpha defaults to 1 (opaque)
@@ -94,7 +94,7 @@ def tile_raster_images(X, img_shape, tile_shape, tile_spacing=(0, 0),
                 dt = out_array.dtype
                 if output_pixel_vals:
                     dt = 'uint8'
-                out_array[:, :, i] = numpy.zeros(
+                out_array[:, :, i] = np.zeros(
                     out_shape,
                     dtype=dt
                 ) + channel_defaults[i]
@@ -115,7 +115,7 @@ def tile_raster_images(X, img_shape, tile_shape, tile_spacing=(0, 0),
         dt = X.dtype
         if output_pixel_vals:
             dt = 'uint8'
-        out_array = numpy.zeros(out_shape, dtype=dt)
+        out_array = np.zeros(out_shape, dtype=dt)
 
         for tile_row in range(tile_shape[0]):
             for tile_col in range(tile_shape[1]):
@@ -139,3 +139,66 @@ def tile_raster_images(X, img_shape, tile_shape, tile_spacing=(0, 0),
                         tile_col * (W + Ws): tile_col * (W + Ws) + W
                     ] = this_img * c
         return out_array
+
+
+
+def tile_raster_color(weights, img_shape, tile_shape, tile_spacing=(1, 1)):
+    out_shape    = [0,0]
+    out_shape[0] = (img_shape[0]+tile_spacing[0])*tile_shape[0] - tile_spacing[0]
+    out_shape[1] = (img_shape[1]+tile_spacing[1])*tile_shape[1] - tile_spacing[1]
+
+    imgs = len(weights[0])
+
+    cols = int(np.sqrt(imgs))
+
+    rows = imgs/cols
+
+    out_img = np.zeros((rows * img_shape[0] * tile_shape[0], cols * img_shape[1] * tile_shape[1],3))
+
+    out_img = np.zeros((cols * img_shape[0] * tile_shape[0] + cols * tile_spacing[0] + tile_spacing[0] , rows * img_shape[1]  * tile_shape[1]+ rows * tile_spacing[1] + tile_spacing[1] ,3))
+
+    for i in range(imgs):
+
+        filteri = np.zeros((img_shape[0] * tile_shape[0], img_shape[1] * tile_shape[1] ,3))
+
+        for h in range(len(weights[i])):
+            for j in range(len(weights[i][0])):
+                filteri[h * tile_shape[0]: h  * tile_shape[0] + tile_shape[0],
+                        j * tile_shape[1]: j  * tile_shape[1] +  tile_shape[1]] = weights[i,h,j,:]
+
+
+        col = int(i / rows)
+
+        offseti = i*img_shape[1] *tile_shape[1] + tile_spacing[1] * i + tile_shape[1] * img_shape[1]
+
+        offsetcol = col*img_shape[0] *tile_shape[0] + tile_spacing[0] * col + tile_shape[0] * img_shape[0]
+
+        #print filteri.shape
+
+        out_img[tile_spacing[1] + col *img_shape[0]*tile_shape[0] + col * tile_spacing[1]: offsetcol + tile_spacing[0], tile_spacing[1]+i  *img_shape[1]*tile_shape[1] + i * tile_spacing[1] : offseti + tile_spacing[1]] = filteri
+
+    print out_img.shape
+
+    return out_img
+
+
+
+
+if __name__ == "__main__":
+
+    X = np.array([
+        [[[255,255,255],[200,20,220], [128,128,128]],
+         [[255,255,255],[220,220,220], [128,128,128]],
+         [[255,255,255],[220,220,220], [128,128,128]]],
+
+        [[[0,0,0],[0,220,0], [128,128,128]],
+         [[255,0,0],[022,0,0], [128,128,128]],
+         [ [255,255,0],[022,0,0], [128,128,128]]],
+
+        [[[255,255,255],[110,0,0], [128,128,128]],
+         [[255,0,0],[0,0,220], [128,128,128]],
+         [[255,255,0],[220,0,0], [128,128,128]]]])
+
+
+    out_img = tile_raster_color(X,(3,3), (10,10),(1,1))
+    cv2.imwrite("out.png", out_img)
