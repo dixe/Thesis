33f2eca445552f4051251fd8fa082631cd7b847f
nikolaj <niko.fris@gmail.com>
2017-08-02 13:38:11 +0200

    finished vis layers of settings to test

--- src/experiments/visualize_layers.py
+++ src/experiments/visualize_layers.py
@@ -1,6 +1,17 @@
 import settings_stats as ss
 import test_network as tn
-
+import Weightstore as ws
 
 if __name__ == "__main__":
-    
+
+
+   settings_file_path = "settings_to_test.txt"
+   settings = list(map(lambda x : str(x.guid), ss.load_settings_from_file(settings_file_path)))
+
+   layers = [0]
+
+   for s in settings:
+      net = ws.get_net(s)
+
+      for l in layers:
+         tn.visualize_weights(net,l)
